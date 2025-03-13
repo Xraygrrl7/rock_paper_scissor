2e3c5075d4190fea80a136e0d8b52cbf0be9fa03
Xraygrrl7 <xraygrrl7@yahoo.com>
2025-03-13 13:25:41 +0100

    Part 4 now working correctly
    lots of trial and error. I sometimes put console.log in the wrong place.
    I found if i didn't comment it out from previous steps it will perform the same task multiple times.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // generate a random number between 0 and 1
-// If number is < 0.33 return rock
-// else if number is < 0.66 return paper
-// else return scissor
+// If number is < 0.33  rock
+// else if number is < 0.66  paper
+// else  scissor
 
 // function getComputerChoice(num) {
 //   if (num < 0.33) {
-//     return "Rock";
+//      "Rock";
 //   } else if (num < 0.66) {
-//     return "Paper";
+//      "Paper";
 //   } else {
-//     return "Scissor";
+//      "Scissor";
 //   }
 //   console.log(num);
 // };
@@ -22,11 +22,11 @@
 
 //     if (num < 0.33) {
 
-//       return "Rock";
+//        "Rock";
 //     } else if (num < 0.66) {
-//       return "Paper";
+//        "Paper";
 //     } else {
-//       return "Scissor";
+//        "Scissor";
 //     }
 
 //   };
@@ -37,25 +37,68 @@ function getComputerChoice() {
   console.log(num);
 
   if (num < 0.33) {
-    return "Rock";
+    return "rock";
   } else if (num < 0.66) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
 }
 console.log(getComputerChoice());
-getComputerChoice();
+// getComputerChoice();
 
-// Part 2 
+// Part 2 logic for getting human choice
 // function getHumanChoice
 // prompt user 'type rock, paper, or scissors to play'
 // store choice
 // End function
 
-function getHumanChoice () {
-  let choice = prompt('To play type rock, paper, or scissors');
-  return choice;
+function getHumanChoice() {
+  let choice;
 
+  while (true) {
+    choice = prompt("To play type rock, paper, or scissors").toLowerCase();
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+      return choice;
+    } else {
+      alert("Invalid choice! Please enter rock, paper, or scissors.");
+    }
+  }
 }
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
+
+// Part 3 declare players score variables
+
+// let humanScore = 0;
+// let computerScore = 0;
+
+// part 4 write logic to play a single round
+//
+function playRound(humanChoice, computerChoice) {
+  console.log(
+    `playRound is running with Human: ${humanChoice}, Computer: ${computerChoice}`
+  );
+
+  if (humanChoice === computerChoice) {
+    alert("It is a draw");
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    alert("you win, scissors beats paper!");
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    alert("you lose, rock beats scissors!");
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    alert("you lose, scissors beats paper!");
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    alert("you win, paper beats rock!");
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    alert("you lose, paper beats rock!");
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    alert("you win, rock beats scissors!");
+  } else {
+    alert("Invalid choice! Please enter rock, paper, or scissors.");
+  }
+}
+const humanSelection = getHumanChoice();
+
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
