e6ab5421e7b6ec60d567b1af29cd58b6a428a7c4
Xraygrrl7 <xraygrrl7@yahoo.com>
2025-03-13 11:11:17 +0100

    Part 2 stores users choice

--- script.js
+++ script.js
@@ -15,38 +15,47 @@
 // };
 // getComputerChoice(0.2);
 
-
 // function getComputerChoice() {
-    
+
 //     let num = Math.random();
 //     console.log(num);
 
 //     if (num < 0.33) {
-      
+
 //       return "Rock";
 //     } else if (num < 0.66) {
 //       return "Paper";
 //     } else {
 //       return "Scissor";
 //     }
-    
+
 //   };
 //   getComputerChoice();
 
-  function getComputerChoice() {
-    
-    let num = Math.random();
-    console.log(num);
+function getComputerChoice() {
+  let num = Math.random();
+  console.log(num);
 
-    if (num < 0.33) {
-      
-      return "Rock";
-    } else if (num < 0.66) {
-      return "Paper";
-    } else {
-      return "Scissor";
-    }
-    
+  if (num < 0.33) {
+    return "Rock";
+  } else if (num < 0.66) {
+    return "Paper";
+  } else {
+    return "Scissors";
   }
-  console.log(getComputerChoice());
-  getComputerChoice();
+}
+console.log(getComputerChoice());
+getComputerChoice();
+
+// Part 2 
+// function getHumanChoice
+// prompt user 'type rock, paper, or scissors to play'
+// store choice
+// End function
+
+function getHumanChoice () {
+  let choice = prompt('To play type rock, paper, or scissors');
+  return choice;
+
+}
+console.log(getHumanChoice());
